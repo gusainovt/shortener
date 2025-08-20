dfd8d67a3f187010d2df55e01c47b801acbb2e62
gusainovt <timurka24478@gmail.com>
2025-08-20 15:55:56 +0300

    add logger

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"shortener/internal/config"
 
+	"github.com/joho/godotenv"
+)
+import "log/slog"
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting shortener", slog.String("env", cfg.Env))
+	log.Debug("debug enabled")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
